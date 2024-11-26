3ecf7936e0d242c7789f8df1644ed0dcfc426e8f
Brandon 'Okidokie98' de Baat <baat0004@hz.nl>
2024-11-26 14:16:23 +0100

    Added CORS to route.js

--- apigateway/code/routes/index.js
+++ apigateway/code/routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 const router = express.Router();
+import cors from 'cors';
+const app = express();
 
 // create a proxy for each microservice
 const microserviceProxy = createProxyMiddleware({
@@ -9,5 +11,6 @@ const microserviceProxy = createProxyMiddleware({
 });
 
 router.use('/microservice', microserviceProxy);
+app.use(cors());
 
 export default router;
