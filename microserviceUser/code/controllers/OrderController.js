15c00dabe2e2c4583e6802e9ef1b8ac3ba4a1062
KleinePliep <adri0066@hz.nl>
2024-12-03 10:20:19 +0100

    little fine tune

--- microserviceUser/code/controllers/OrderController.js
+++ microserviceUser/code/controllers/OrderController.js
@@ -2,15 +2,15 @@ import { JSONFilePreset } from "lowdb/node";
 
 // Initialize database with default structure
 const defaultData = {
-  meta: { tile: "List of users", date: "November 2024" },
-  users: [],
-  orders: [],
+  meta: { tile: "List of Users", date: "November 2024" },
+  Users: [],
+  Orders: [],
 };
-const db = await JSONFilePreset("users.json", defaultData);
-const orders = db.data.orders;
+const db = await JSONFilePreset("Users.json", defaultData);
+const Orders = db.data.Orders;
 
 export async function getAllOrders(req, res) {
-  return res.status(200).json(orders);
+  return res.status(200).json(Orders);
 }
 
 export async function createOrder(req, res) {
@@ -22,21 +22,21 @@ export async function createOrder(req, res) {
       .send(`UserID, Amount, and PaymentDate are required inputs.`);
   }
 
-  let user = db.data.users.find((u) => u.UserID === UserID);
+  let user = db.data.Users.find((u) => u.UserID === UserID);
 
   if (!user) {
     return res.status(404).send(`No user found with ID ${UserID}`);
   }
 
   let newOrder = {
-    OrderID: orders.length + 1,
+    OrderID: Orders.length + 1,
     DiscountCodeID,
     UserID,
     Amount,
     PaymentDate,
   };
 
-  orders.push(newOrder);
+  Orders.push(newOrder);
   user.OrderID = newOrder.OrderID; // Link order to user
   await db.write();
 
@@ -53,7 +53,7 @@ export async function getOrderById(req, res) {
     return res.status(400).send(`Order ID is required.`);
   }
 
-  let order = orders.find((o) => o.OrderID === id);
+  let order = Orders.find((o) => o.OrderID === id);
 
   if (!order) {
     return res.status(404).send(`No order found with ID ${id}`);
--- microserviceUser/code/routes/index.js
+++ microserviceUser/code/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createUser, getAllUsers } from "../controllers/UserController.js";
+import { getUserWithOrders } from "../controllers/OrderController.js";
 import { checkName } from "../middleware/exampleMiddleware.js";
 const router = express.Router();
 
@@ -10,5 +11,10 @@ router.get("/", (req, res) => {
 
 router.get("/users", checkName, getAllUsers);
 router.post("/users/add", checkName, createUser);
+
+/*
+ * Start of Order routes
+ */
+router.get("/orderByUser", checkName, getUserWithOrders);
 
 export default router;
