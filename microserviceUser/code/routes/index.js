eff1873b413a05f1123ae8cfe80b9f2bf0485c6d
KleinePliep <adri0066@hz.nl>
2024-12-06 13:07:14 +0100

    added boolean change and routes for the user/orders

--- microserviceUser/code/routes/index.js
+++ microserviceUser/code/routes/index.js
@@ -4,6 +4,11 @@ import {
   getAllUsers,
   getUserWithOrders,
 } from "../controllers/UserController.js";
+import {
+  getAllOrders,
+  createOrder,
+  getOrderById,
+} from "../controllers/OrderController.js";
 import { checkName } from "../middleware/exampleMiddleware.js";
 const router = express.Router();
 
@@ -14,10 +19,13 @@ router.get("/", (req, res) => {
 
 router.get("/users", checkName, getAllUsers);
 router.post("/users/add", checkName, createUser);
+router.get("/orderByUser/:id", checkName, getUserWithOrders);
 
 /*
  * Start of Order routes
  */
-router.get("/orderByUser/:id", checkName, getUserWithOrders);
+router.get("/allOrders", checkName, getAllOrders);
+router.get("/Order/:id", checkName, getOrderById);
+router.post("/Order/add", checkName, createOrder);
 
 export default router;
