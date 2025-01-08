e35fca2408eab91e369cd0f841c1d992fc6e474a
Brandon 'Okidokie98' de Baat <baat0004@hz.nl>
2025-01-08 14:22:32 +0100

    Route updates

--- microserviceUser/code/routes/index.js
+++ microserviceUser/code/routes/index.js
@@ -10,22 +10,24 @@ import {
   getOrderById,
 } from "../controllers/OrderController.js";
 import { checkName } from "../middleware/exampleMiddleware.js";
+
 const router = express.Router();
 
-// routes
+// Root route
 router.get("/", (req, res) => {
-  res.json("hi, I am the Microservice User");
+  res.json("Hi, I am the Microservice User");
 });
 
+// User routes
 router.get("/users", checkName, getAllUsers);
-router.post("/users/add", checkName, createUser);
-router.get("/orderByUser/:id", checkName, getUserWithOrders);
+router.post("/users", checkName, createUser); // POST zonder /add
+router.get("/users/:id/orders", checkName, getUserWithOrders); // Gebruik :id voor dynamische ID en duidelijke route
 
 /*
  * Start of Order routes
  */
-router.get("/allOrders", checkName, getAllOrders);
-router.get("/Order/:id", checkName, getOrderById);
-router.post("/Order/add", checkName, createOrder);
+router.get("/orders", checkName, getAllOrders); // Meervoud en duidelijker
+router.get("/orders/:id", checkName, getOrderById); // Gebruik /orders/:id voor het ophalen van orders
+router.post("/orders", checkName, createOrder); // POST zonder /add
 
 export default router;
