3476bff752ef7d848e2b8fc62696f8a7579bbd1d
KleinePliep <adri0066@hz.nl>
2025-01-20 11:27:52 +0100

    will it work????

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        jest: true, // Add Jest globals
+        describe: true,
+        expect: true,
+        it: true,
+        beforeEach: true,
+        afterEach: true,
+        beforeAll: true,
+        afterAll: true,
+      },
+    },
+  },
   pluginJs.configs.recommended,
+  "plugin:jest/recommended",
 ];
