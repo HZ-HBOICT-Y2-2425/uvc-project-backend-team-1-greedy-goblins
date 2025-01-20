c7e4045307d3b7eade7f1563ce1d38c1595289c5
Hoo-dini <144112895+Kylianvanhelden@users.noreply.github.com>
2025-01-20 13:21:02 +0100

    Revert "CIN Toevoegingen"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginJest from "eslint-plugin-jest";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    plugins: {
-      pluginJest: pluginJest,
-      pluginJs: pluginJs,
-    },
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        jest: true,
-        describe: true,
-        expect: true,
-        it: true,
-        beforeEach: true,
-        afterEach: true,
-        beforeAll: true,
-        afterAll: true,
-      },
-    },
-  },
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
 ];
